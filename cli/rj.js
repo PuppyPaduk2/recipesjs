35b13dd363600b324ce1eace4dfcc3035116702e
puppypaduk2 <debraooclark@gmail.com>
2022-03-15 15:23:45 +0300

    feat: Add flag to parse command

--- cli/rj.js
+++ cli/rj.js
@@ -7,23 +7,28 @@ const { version } = require("../package.json");
 const { parsePackage } = require("../utils/parse-package");
 const { printTerminal } = require("../utils/print-terminal");
 
-program.command("parse").action(async () => {
-  const packageFile = resolve(process.cwd(), "./package.json");
-  const { installDeps, installDevDeps } = await parsePackage(packageFile);
+program
+  .command("parse")
+  .option("--is-dep-version", "Use dependency version of packages")
+  .action(async ({ isDepVersion }) => {
+    const packageFile = resolve(process.cwd(), "./package.json");
+    const { installDeps, installDevDeps } = await parsePackage(packageFile, {
+      isDepVersion,
+    });
 
-  printTerminal(
-    [
-      installDeps && {
-        title: "Dependencies:",
-        describe: installDeps,
-      },
-      installDevDeps && {
-        before: "",
-        title: "Dev dependencies:",
-        describe: installDevDeps,
-      },
-    ].filter(Boolean)
-  );
-});
+    printTerminal(
+      [
+        installDeps && {
+          title: "Dependencies:",
+          describe: installDeps,
+        },
+        installDevDeps && {
+          before: "",
+          title: "Dev dependencies:",
+          describe: installDevDeps,
+        },
+      ].filter(Boolean)
+    );
+  });
 
 program.version(version).parse(process.argv);
--- utils/parse-package.js
+++ utils/parse-package.js
@@ -1,22 +1,23 @@
 const { readJson } = require("./read-json");
 
-async function parsePackage(path) {
+async function parsePackage(path, { isDepVersion = false } = {}) {
   const json = await readJson(path);
 
   return {
-    installDeps: buildDeps(json.dependencies || {}),
-    installDevDeps: buildDeps(json.devDependencies || {}),
+    installDeps: buildDeps(json.dependencies || {}, { isDepVersion }),
+    installDevDeps: buildDeps(json.devDependencies || {}, { isDepVersion }),
   };
 }
 
-function buildDeps(deps) {
+function buildDeps(deps, { isDepVersion = false } = {}) {
   const entries = Object.entries(deps);
   let instruction = "";
 
   for (const index in entries) {
     const [key, value] = entries[index];
 
-    instruction += `  ${key}@${value}`;
+    if (isDepVersion) instruction += `  ${key}@${value}`;
+    else instruction += `  ${key}`;
 
     if (+index !== entries.length - 1) instruction += " \\\n";
   }
